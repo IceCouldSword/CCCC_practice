fdf492c5d4f9ca1304a662215e3c82592d04fffa
IceCouldSword <958262492@qq.com>
2020-11-16 18:33:26 +0800

    123

--- L1-031.cpp
+++ L1-031.cpp
@@ -3,10 +3,5 @@ using namespace std;
 int main()
 {
     cout << "hello world" << endl;
-    cout << "yes" << endl;
-    cout << "yes" << endl;
-    cout << "yes" << endl;
-    cout << "yes" << endl;
-    cout << "yes" << endl;
     system("pause");
 }
